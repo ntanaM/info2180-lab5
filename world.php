23f8acea7f69ff57e2eade77759b59e98ebcd436
Dana Archer <danamarcher@gmail.com>
2024-12-01 03:00:18 -0500

    Updated World.php

--- world.php
+++ world.php
@@ -1,13 +1,26 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_GET['country'])){
+  $userQuery = $_GET['country'];
+
+  # Country lookup
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $stmt->bindValue(':country', '%'. $userQuery . '%', PDO::PARAM_STR);
+  $stmt->execute();
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+
+}
+
+
+
 
 ?>
 <ul>

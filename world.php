0e65fc0a2e4fc8cb4b2c9516f21aabf7649608df
Dana Archer <danamarcher@gmail.com>
2024-12-01 03:41:22 -0500

    Code for Exercise 4

--- world.php
+++ world.php
@@ -23,8 +23,29 @@ if (isset($_GET['country'])){
 
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table class = "table">
+  <thead>
+    <tr>
+      <th>Country</th>
+      <th>Continent</th>
+      <th>Independence Year </th>
+      <th> Head of State </th>
+
+</tr>
+</thead>
+
+<tbody>
+  <?php
+  foreach($results as $row):?>
+  <tr>
+    <td> <?= $row['name']?></td>
+    <td> <?= $row['continent']?></td>
+    <td> <?= $row['independence_year']?></td>
+    <td> <?= $row['head_of_state']?></td>
+  </tr>
+    
+  <?php endforeach; ?>
+
+</tbody>
+</table>

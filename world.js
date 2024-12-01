7592f7026bee4fd977a59546f7abef66b7a28059
Dana Archer <danamarcher@gmail.com>
2024-12-01 03:03:30 -0500

    Updated js file

---
+++ world.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Code for Exercise 3
+    // Get Elements
+
+    const button = document.getElementById("lookup");
+    const result = document.getElementById("result");
+    const userInput = document.getElementById("country");
+
+    function buttonListener(){
+        const userQuery = userInput.value.toLowerCase();
+        const userRequest = new XMLHttpRequest();
+        userRequest.open(`GET`, `world.php?country=${encodeURIComponent(userQuery)}`, true);
+        userRequest.onreadystatechange = function(){
+            if(userRequest.readyState === XMLHttpRequest.DONE){
+                if(userRequest.status === 200){
+                    alert("This is working");
+                }
+
+                else{
+                    alert("There was a problem processing your request.");
+                }
+            }
+        }
+        userRequest.send();
+    };
+
+
+    button.addEventListener('click', buttonListener());
+
+});
